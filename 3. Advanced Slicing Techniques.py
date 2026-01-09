298d3f135c4d79302e4a720de2c7fb6c8056581e
walmartjobluke-ops <walmartjobluke@gmail.com>
2026-01-08 23:21:37 -0600

    Add files via upload

---
+++ 3. Advanced Slicing Techniques.py
@@ -0,0 +1,20 @@
+#Objective: The aim of this assignment is to use Python list slicing.
+
+#Problem Statement: You have a list of daily temperatures for one month, and you'd like to extract specific data from it.
+
+#Task 1: Given the list of temperatures:
+
+#temperatures = [72, 75, 78, 79, 80, 81, 82, 83, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106]
+#Extract the temperatures for the second week (7 days) of the month (index 7 to index 14). 
+#Expected Outcome:
+
+#83, 85, 86, 87, 88, 89, 90
+
+temperatures = [72, 75, 78, 79, 80, 81, 82, 83, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106]
+second_week_temps = temperatures[7:14]
+print("Temperatures for the second week:", second_week_temps)
+
+#Task 2: Extract all the temperatures above 100. HINT: add the temperatures over 100 to a new list, or use list slicing to get the temperatures.
+
+above_100_temps = [temp for temp in temperatures if temp > 100]
+print("Temperatures above 100:", above_100_temps)
